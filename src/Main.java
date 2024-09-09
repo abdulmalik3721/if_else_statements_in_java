4bac68cb82f0506f10951b64baf75a443b0aaf8a
Faiz ansari <malik.ksg@gmail.com>
2024-09-09 10:24:22 +0530

    changed fileds(variables)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int bonus =500;
-        boolean isScore =true;
-        if(bonus<500){
-            System.out.println("your score less than 500");
+        boolean gameOver =true;
+        int score =5000;
+        int levelCompleted =5;
+        int bonus =100;
+
+        if(score<5000){
+            System.out.println("your score was less than 5000");
         }
         else{
-            System.out.println("got it");
+            System.out.println("Go here");
         }
     }
 }
