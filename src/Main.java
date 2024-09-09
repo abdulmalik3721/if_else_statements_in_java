3903d50284da18f175da2949099a329be819272e
taha <malik.kasganj@gmail.com>
2024-09-09 10:18:03 +0530

    if else added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus =500;
+        boolean isScore =true;
+        if(bonus<500){
+            System.out.println("your score less than 500");
+        }
+        else{
+            System.out.println("got it");
+        }
+    }
+}
